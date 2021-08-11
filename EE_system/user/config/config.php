ae3513ad4ecc04922a1afe4a8fc33007d304282b
“voltasdev01” <voltasdev01@voltas.com>
2021-08-11 09:52:04 +0000

    Changes db to localhost

--- EE_system/user/config/config.php
+++ EE_system/user/config/config.php
@@ -78,7 +78,7 @@ switch($_SERVER['HTTP_HOST']) {
     break;
     case 'devvoltas.voltasworld.com' || '172.16.9.237':
         $dbConnection = array (
-            'hostname' => '172.16.9.237',
+            'hostname' => 'localhost',
 			'database' => 'voltas_web',
 			'username' => 'voltas_web_user',
 			'password' => 'Password@123',
